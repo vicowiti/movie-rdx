b599fc71985a83d392a8d925dd62dd8e9798d22c
Victor Owiti <viowiti12@gmail.com>
2023-03-22 12:37:27 +0300

    Mvp done

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import store from "./app/store";
-import { moviesApi } from "./features/movieSlice";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
